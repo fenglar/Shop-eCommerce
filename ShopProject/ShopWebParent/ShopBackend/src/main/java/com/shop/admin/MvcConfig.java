22939c0f162b56fd5c2b8d2364882a5e2c17675c
weklar10@gmail.com <weklar10@gmail.com>
2021-08-23 17:50:23 -0700

    code category form

--- ShopProject/ShopWebParent/ShopBackend/src/main/java/com/shop/admin/MvcConfig.java
+++ ShopProject/ShopWebParent/ShopBackend/src/main/java/com/shop/admin/MvcConfig.java
@@ -1,6 +1,5 @@
 package com.shop.admin;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
