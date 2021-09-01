19e51fecf383f401fa9a81d5e23c0f3e293e5f63
weklar10@gmail.com <weklar10@gmail.com>
2021-09-01 16:09:28 -0700

    imported sample category data

--- ShopProject/ShopWebParent/ShopBackend/src/main/java/com/shop/admin/category/CategoryRepository.java
+++ ShopProject/ShopWebParent/ShopBackend/src/main/java/com/shop/admin/category/CategoryRepository.java
@@ -2,7 +2,6 @@ package com.shop.admin.category;
 
 
 import com.shop.common.entity.Category;
-import com.shop.common.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -19,6 +18,9 @@ public interface CategoryRepository extends PagingAndSortingRepository<Category,
 
     @Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
     public Page<Category> findRootCategories(Pageable pageable);
+
+    @Query("SELECT c FROM Category c WHERE c.name LIKE %?1%")
+    public Page<Category> search(String keyword, Pageable pageable);
 
     public Long countById(Integer id);
 
