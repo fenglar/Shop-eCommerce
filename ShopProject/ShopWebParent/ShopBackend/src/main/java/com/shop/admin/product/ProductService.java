3c215ad966f82ecf4848e4acd204aa5075c319ad
Marcin Weklar <marcin@marcins-macbook-air.home>
2021-09-13 14:16:17 +0200

    save product function

--- ShopProject/ShopWebParent/ShopBackend/src/main/java/com/shop/admin/product/ProductService.java
+++ ShopProject/ShopWebParent/ShopBackend/src/main/java/com/shop/admin/product/ProductService.java
@@ -4,6 +4,7 @@ import com.shop.common.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -16,4 +17,18 @@ public class ProductService {
         return (List<Product>) repo.findAll();
     }
 
+    public Product save(Product product) {
+        if (product.getId()==null) {
+            product.setCreatedTime(new Date());
+        }
+        if(product.getAlias() == null || product.getAlias().isEmpty()) {
+            String defaultAlias = product.getName().replaceAll(" ", "-");
+            product.setAlias(defaultAlias);
+        } else {
+            product.setAlias(product.getAlias().replaceAll(" ", "-"));
+        }
+        product.setUpdatedTime(new Date());
+
+        return repo.save(product);
+    }
 }
