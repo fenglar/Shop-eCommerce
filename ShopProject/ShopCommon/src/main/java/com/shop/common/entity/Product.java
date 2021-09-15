6770f8e078f2451c068665188d1658cb6b13fd86
Marcin Weklar <weklar10@gmail.com>
2021-09-15 22:56:58 +0200

    add Details on product form

--- ShopProject/ShopCommon/src/main/java/com/shop/common/entity/Product.java
+++ ShopProject/ShopCommon/src/main/java/com/shop/common/entity/Product.java
@@ -1,9 +1,7 @@
 package com.shop.common.entity;
 
 import javax.persistence.*;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Table(name = "products")
@@ -52,6 +50,8 @@ public class Product {
     private Brand brand;
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
     private Set<ProductImage> images = new HashSet<>();
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+    private List<ProductDetail> details = new ArrayList<>();
 
     public Product() {
     }
@@ -257,5 +257,13 @@ public class Product {
         if (id == null || mainImage == null) return "/images/image-thumbnail.png";
 
         return "/product-images/" + this.id + "/" + this.mainImage;
+    }
+
+    public List<ProductDetail> getDetails() {
+        return details;
+    }
+
+    public void addDetail(String name, String value) {
+        this.details.add(new ProductDetail(name, value, this));
     }
 }
