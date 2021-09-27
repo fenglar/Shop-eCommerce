9854a35dbdbb9a8525f2ea6ae4c25efd161f0fa4
Marcin Weklar <weklar10@gmail.com>
2021-09-27 13:40:59 +0200

    currency entity, repository and unit tests

---
+++ ShopProject/ShopWebParent/ShopBackend/src/main/java/com/shop/site/admin/setting/CurrencyRepository.java
@@ -0,0 +1,8 @@
+package com.shop.site.admin.setting;
+
+import com.shop.site.common.entity.Currency;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CurrencyRepository extends CrudRepository<Currency, Integer> {
+
+}
