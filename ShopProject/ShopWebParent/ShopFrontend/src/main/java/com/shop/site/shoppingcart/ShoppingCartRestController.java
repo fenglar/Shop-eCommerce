97ae64ee49e6d72ad15b0060965f7d2f1e1a23ce
Marcin Weklar <weklar10@gmail.com>
2021-10-15 19:00:06 +0200

    update product quantity

--- ShopProject/ShopWebParent/ShopFrontend/src/main/java/com/shop/site/shoppingcart/ShoppingCartRestController.java
+++ ShopProject/ShopWebParent/ShopFrontend/src/main/java/com/shop/site/shoppingcart/ShoppingCartRestController.java
@@ -42,4 +42,17 @@ public class ShoppingCartRestController {
         return customerService.getCustomerByEmail(email);
     }
 
+    @PostMapping("/cart/update/{productId}/{quantity}")
+    public String updateQuantity(@PathVariable(name = "productId") Integer productId,
+                                 @PathVariable("quantity") Integer quantity, HttpServletRequest request) {
+        try {
+            Customer customer = getAuthenticatedCustomer(request);
+            Float subtotal = cartService.updateQuantity(productId, quantity, customer);
+            return String.valueOf(subtotal);
+
+        } catch (CustomerNotFoundException e) {
+            return "You must login to change quantity of product cart";
+        }
+    }
 }
+
--- ShopProject/ShopWebParent/ShopFrontend/src/main/java/com/shop/site/shoppingcart/ShoppingCartService.java
+++ ShopProject/ShopWebParent/ShopFrontend/src/main/java/com/shop/site/shoppingcart/ShoppingCartService.java
@@ -3,25 +3,32 @@ package com.shop.site.shoppingcart;
 import com.shop.site.common.entity.CartItem;
 import com.shop.site.common.entity.Customer;
 import com.shop.site.common.entity.Product;
+import com.shop.site.product.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class ShoppingCartService {
     @Autowired
+    private
     CartItemRepository cartRepo;
+    @Autowired
+    private ProductRepository productRepo;
 
-    public Integer addProduct(Integer productId, Integer quantity, Customer customer) throws ShoppingCartException{
+    public Integer addProduct(Integer productId, Integer quantity, Customer customer) throws ShoppingCartException {
         Integer updatedQuantity = quantity;
         Product product = new Product(productId);
         CartItem cartItem = cartRepo.findByCustomerAndProduct(customer, product);
         if (cartItem != null) {
             updatedQuantity = cartItem.getQuantity() + quantity;
-            if(updatedQuantity > 5){
+            if (updatedQuantity > 5) {
                 throw new ShoppingCartException("Could not add more " + quantity + " item(s)" +
-                        "because there's already "+ cartItem.getQuantity()+" item(s) in your shopping cart. " +
+                        "because there's already " + cartItem.getQuantity() + " item(s) in your shopping cart. " +
                         "Maximum quantity allowed is 5.");
             }
         } else {
@@ -38,6 +45,13 @@ public class ShoppingCartService {
 
     public List<CartItem> listCartItems(Customer customer) {
         return cartRepo.findByCustomer(customer);
+    }
+
+    public float updateQuantity(Integer productId, Integer quantity, Customer customer) {
+        cartRepo.updateQuantity(quantity, customer.getId(), productId);
+        Product product = productRepo.findById(productId).get();
+        float subtotal = product.getDiscountPercent()*quantity;
+        return subtotal;
     }
 }
 
