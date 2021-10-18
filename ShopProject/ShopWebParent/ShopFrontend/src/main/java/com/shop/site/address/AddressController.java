7d74e9c03246d7b7443b813d6976463436a2d672
Marcin Weklar <weklar10@gmail.com>
2021-10-18 10:36:07 +0200

    addresses entity, repository, servie and controller

---
+++ ShopProject/ShopWebParent/ShopFrontend/src/main/java/com/shop/site/address/AddressController.java
@@ -0,0 +1,4 @@
+package com.shop.site.address;
+
+public class AddressController {
+}
---
+++ ShopProject/ShopWebParent/ShopFrontend/src/main/java/com/shop/site/address/AddressRepository.java
@@ -0,0 +1,21 @@
+package com.shop.site.address;
+
+import com.shop.site.common.entity.Address;
+import com.shop.site.common.entity.Customer;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface AddressRepository extends CrudRepository<Address, Integer> {
+
+    public List<Address> findByCustomer(Customer customer);
+
+    @Query("SELECT a FROM Address a WHERE a.id = ?1 AND a.customer.id = ?2")
+    public Address findByIdAndCustomer(Integer addressId, Integer customerId);
+
+    @Query("DELETE FROM Address a WHERE a.id = ?1 AND a.customer.id = ?2")
+    @Modifying
+    public void deleteByIdAndCustomer(Integer addressId, Integer customerId);
+}
